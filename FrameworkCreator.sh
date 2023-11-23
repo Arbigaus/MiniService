2c2606d9c1cc1768a8d0d20a27a2c79b7ca10db2
Gerson Arbigaus <gerson87@gmail.com>
2023-11-23 14:35:11 -0300

    Removed the zip xcframework file in the script

--- FrameworkCreator.sh
+++ FrameworkCreator.sh
@@ -20,11 +20,4 @@ xcodebuild -create-xcframework \
     -framework ./build/$SchemeName-macOS.xcarchive/Products/usr/local/lib/MiniService.framework \
     -output ./$SchemeName.xcframework
 
-echo "Creating the zip file"
-zip ./$SchemeName.zip ./$SchemeName.xcframework
-echo "File compacted succefull"
-
-echo "Removing the build folder and the framework file"
-rm -rf ./build/ ./$SchemeName.xcframework
-echo "Build folder and framework file removed"
 
