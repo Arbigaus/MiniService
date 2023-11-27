5ab97a9716d40a177001d6958fc2c32843288f52
Gerson Arbigaus <gerson87@gmail.com>
2023-11-27 11:15:39 -0300

    Fixed the `Package.swift` file to use the `binaryTarget`

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MiniService",
-            type: .dynamic,
-            targets: ["MiniService"]),
+//            type: .dynamic,
+            targets: ["MiniServiceSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
